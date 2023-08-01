76953868f1478f465d73ff36e268ff9735be550d
RazielMartelus96 <razielmartelus@curiosity-core.com>
2023-08-01 13:22:38 +0100

    Forgot import for UUID in Entity.java

--- src/main/java/io/curiositycore/chunkcomber/model/entities/Entity.java
+++ src/main/java/io/curiositycore/chunkcomber/model/entities/Entity.java
@@ -3,6 +3,8 @@ package io.curiositycore.chunkcomber.model.entities;
 import io.curiositycore.chunkcomber.model.Deletable;
 import io.curiositycore.chunkcomber.model.Scanable;
 
+import java.util.UUID;
+
 public interface Entity extends Scanable, Deletable {
 
     UUID getEntityId();
